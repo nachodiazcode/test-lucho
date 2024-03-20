40d72229c5d356c3dec00f22d533602c0a911fba
nachodiazcode <ignaciosergiodiaz@gmail.com>
2024-03-20 00:55:09 -0300

    delete HeaderComponent temprorary

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css'
 import ChartComponent from './components/ChartComponent'
-import HeaderComponent from './components/HeaderComponent'
 
 function App() {
   return (
     <>
-      <HeaderComponent />
       <ChartComponent title="" />
     </>
   )
